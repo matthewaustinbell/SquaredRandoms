3d2a1ed273601ef577b68d1f112303507fc35da3
unknown <matthewaustinbell@gmail.com>
2019-09-10 20:32:00 -0500

    added git ignore

---
+++ SquaredRandoms/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SquaredRandoms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(0, 50);
+
+            Console.WriteLine(randomNumber);
+            
+        }
+    }
+}
